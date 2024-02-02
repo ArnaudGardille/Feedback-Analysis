9a8fda625cd664139c1f3ecb591f65adeb9bbb8f
ArnaudGardille <arnaud.gardille@gmail.com>
2024-02-02 15:42:26 +0100

    prompts externalisation

---
+++ src/bubble.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+def deduce_backend_type(insight_type):
+    if insight_type == "1698433300252x835626794232717300":
+        return "pain"
+    elif insight_type == "1698433290120x936044292663509300":
+        return "positive"   
+    elif insight_type == "1698433314230x619003097145126100":
+        return "feature"  
+    elif insight_type == "1698433323222x402426615286320700":
+        return "bug"   
+    print("Incorrect type:", insight_type)
+
+
+
+
+
+    
