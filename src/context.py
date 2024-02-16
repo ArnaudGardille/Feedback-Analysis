410da65c2701d45255dffd9e97838586e5a9a06d
ArnaudGardille <arnaud.gardille@gmail.com>
2024-02-16 12:20:55 +0100

    batched_aspects

--- src/context.py
+++ src/context.py
@@ -1,4 +1,16 @@
 context = {
+    "company": "Darty",
+    "project": "Darty_trustpilot",
+    "context": None,
+    }
+
+context_darty = {
+    "company": "Darty",
+    "project": "Darty_trustpilot",
+    "context": None,
+    }
+
+context_amazon = {
     "company": "www.amazon.com",
     "project": "Amazon_trustpilot",
     "context": None,
--- src/models.py
+++ src/models.py
@@ -11,15 +11,29 @@ TEMPERATURE = 0.2
 EMBEDDING_DIMENSION = 10
 CUSTOM_ENBEDDING_MODEL = False
 
-LLL_PROVIDER = "MISTRAL_AI"
-MISTRAL_API_KEY = "GFBjsGogmbv0LuMWjJewXBXwyN7QeKNj"
-
+LLL_PROVIDER = "OPEN_AI"
+AZURE = False
 
 if LLL_PROVIDER == "OPEN_AI":
-    from openai import OpenAI, AsyncOpenAI
-    client = AsyncOpenAI()
+    EMBEDDING_ENGINE = "text-embedding-3-large"
+    if AZURE:
+        from openai import AzureOpenAI, AsyncAzureOpenAI
+        client = AsyncAzureOpenAI(azure_endpoint="https://vigieinstance.openai.azure.com/",
+        api_version="2023-07-01-preview",
+        api_key="6e612f025340400d827a519b0549cff6")
+        GENERATION_ENGINE = "dep"
+    
+    else:
+        from openai import OpenAI, AsyncOpenAI
+        client = AsyncOpenAI(
+            api_key="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",
+            organization="org-EYbk8L8UD8kpRGOeDarxXD55",
+        )
+        GENERATION_ENGINE = "gpt-4-turbo-preview"
 
-    async def get_async_analysis(client, prompt, response_model):
+    client = instructor.patch(client)
+
+    async def get_async_analysis(prompt, response_model):
         response: response_model = await client.chat.completions.create(
             messages=[
                 {"role": "system", "content": "Tu est un assistant spélialisé dans l'analyse de commentaires, et qui ne renvoit que des fichiers JSON."},
@@ -28,7 +42,7 @@ if LLL_PROVIDER == "OPEN_AI":
             response_format={ "type": "json_object" },
             model=GENERATION_ENGINE,
             temperature=TEMPERATURE,
-            max_retries=MAX_RETRIES,
+            #max_retries=MAX_RETRIES,
             response_model=response_model,
             )
         return response #.choices[0].message.content
@@ -41,33 +55,34 @@ if LLL_PROVIDER == "OPEN_AI":
         res =  loop.run_until_complete(asyncio.gather(*tasks))
         return res
     
+    
+    
 elif LLL_PROVIDER == "MISTRAL_AI":
-    from mistralai.async_client import MistralAsyncClient
+    MISTRAL_API_KEY = "GFBjsGogmbv0LuMWjJewXBXwyN7QeKNj"
+    EMBEDDING_ENGINE = ""#"text-embedding-3-large"
+    GENERATION_ENGINE = "mistralai/Mistral-7B-Instruct-v0.2"
+
+    from mistralai.async_client import MistralClient
     from mistralai.models.chat_completion import ChatMessage
 
     api_key = MISTRAL_API_KEY
     model = "mistral-tiny"
 
-    client = MistralAsyncClient(api_key=api_key)
+    client = MistralClient(api_key=api_key)
     
 
     messages = [
         ChatMessage(role="user", content="What is the best French cheese?")
     ]
 
-    # With async
-    async_response = client.chat(model=model, messages=messages, temperature=TEMPERATURE)
 
-    async for chunk in async_response: 
-        print(chunk.choices[0].delta.content)
-
-    async def get_async_analysis(client, prompt, response_model):
+    async def get_async_analysis(prompt, response_model):
         response: response_model = await client.chat.completions.create(
             messages=[
                 {"role": "system", "content": "Tu est un assistant spélialisé dans l'analyse de commentaires, et qui ne renvoit que des fichiers JSON."},
                 {"role": "user", "content": str(prompt)},
             ],
-            response_format={ "type": "json_object" },
+            #response_format={ "type": "json_object" },
             model=GENERATION_ENGINE,
             temperature=TEMPERATURE,
             max_retries=MAX_RETRIES,
@@ -91,48 +106,25 @@ else:
 if CUSTOM_ENBEDDING_MODEL:
     from sentence_transformers import SentenceTransformer
     embedding_model = SentenceTransformer('OrdalieTech/Solon-embeddings-large-0.1')
-GENERATION_ENGINE = "gpt-4-turbo-preview"
-EMBEDDING_ENGINE = "text-embedding-3-large"
-
-GENERATION_MODEL = "mistralai/Mistral-7B-Instruct-v0.2"
 
 
 
-client = instructor.patch(client)
+def get_analysis(prompt, response_model):
+    return apply_async_analysis([prompt], response_model)[0]
 
 
 
 #%% LLMs
 
-async def get_async_analysis(client, prompt, response_model):
-    response: response_model = await client.chat.completions.create(
-        messages=[
-            {"role": "system", "content": "Tu est un assistant spélialisé dans l'analyse de commentaires, et qui ne renvoit que des fichiers JSON."},
-            {"role": "user", "content": str(prompt)},
-        ],
-        response_format={ "type": "json_object" },
-        model=GENERATION_ENGINE,
-        temperature=TEMPERATURE,
-        max_retries=MAX_RETRIES,
-        response_model=response_model,
-        )
-    return response #.choices[0].message.content
 
-def apply_async_analysis(prompts, response_models):
-    if type(response_models) is not list:
-        response_models = [response_models for _ in prompts]
-    loop = asyncio.get_event_loop()
-    tasks = [loop.create_task(get_async_analysis(prompt, response_model)) for (prompt, response_model) in zip(prompts, response_models)]
-    res =  loop.run_until_complete(asyncio.gather(*tasks))
-    return res
-
+"""
 def get_analysis(prompt, response_model):
     response = client.chat.completions.create(
         messages=[
             #{"role": "system", "content": "Tu est analyste marketing"},
             {"role": "user", "content": str(prompt)},
         ],
-        model=GENERATION_MODEL,
+        model=GENERATION_ENGINE,
         #response_format={ "type": "json_object" },
         response_model=response_model,
         )
@@ -149,3 +141,36 @@ def apply_analysis(prompts, response_models, bar=False):
     for (prompt, response_model) in iterator:
         res.append(get_analysis(prompt, response_model))
     return res
+    """
+
+
+"""
+def tag_single_request(prompt: str, sous_categories: List[SousCategorie]) -> Aspect:
+    allowed_tags = [(tag.indice, tag.nom) for tag in sous_categories]
+    allowed_tags_str = ", ".join([f"`{tag}`" for tag in allowed_tags])
+
+    return client.chat.completions.create(
+        model="mixtral",
+        messages=[
+            {
+                "role": "system",
+                "content": f"Tu es {feedback_context['role']} au sein de l'entreprise {feedback_context['entreprise']}. Voici un bref rappel sur cette entreprise: \n'{feedback_context['context']}'\n\En tant que  {feedback_context['role']}, tu est spécialisé dans l'analyse de commentaire."
+            },
+            {"role": "user", "content": prompt},
+            {
+                "role": "user",
+                "content": f"Voici les sous-catégories: {allowed_tags_str}",
+            },
+        ], 
+        response_model=ListAspects,  # Minimizes the hallucination of tags that are not in the allowed tags.
+        validation_context={"sous_categories": sous_categories},
+    )
+
+def tag_request(request: AspectsRequest) -> AspectsResponse:
+    predictions = [tag_single_request(text, request.tags) for text in request.texts]
+    
+    return AspectsResponse(
+        texts=request.texts,
+        predictions=predictions)
+
+"""
